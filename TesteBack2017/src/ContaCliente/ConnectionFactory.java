7c26b2a5b255f140fcc8094b730891ec79818b5b
Felipe Machado <felipe.machado1992@hotmail.com>
2017-09-10 16:21:23 -0300

    Criação da Classe Connection Factory

---
+++ TesteBack2017/src/ContaCliente/ConnectionFactory.java
@@ -0,0 +1,20 @@
+package ContaCliente;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Felipe
+ */
+public class ConnectionFactory {
+    public Connection getConnection() throws Exception{
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/customerdb", "root", "root");
+        } catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
